8c031b292d06cf15937285430e99288509e32544
mikemoreedu <81551332+mikemoreedu@users.noreply.github.com>
2023-02-24 22:41:35 -0500

    Update test_Github.py

--- test_Github.py
+++ test_Github.py
@@ -3,7 +3,7 @@ import Github
 
 class github_test(unittest.TestCase):
     def test_Valid_output(self):
-        self.assertEqual(Github.get_git_repos_and_commits("mikemoreedu"), [('SSW567CircleCI', 11), ('Triangle567', 5)])
+        self.assertEqual(Github.get_git_repos_and_commits("mikemoreedu"), [('SSW567CircleCI', 9), ('Triangle567', 5)])
 
     def test_bad_input(self):
         self.assertEqual(Github.get_git_repos_and_commits(1), "User ID must be a string")
