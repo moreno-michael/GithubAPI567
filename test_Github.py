6ed6ff0d333352c14dbe32d9e6edb7d735e7b50e
mikemoreedu <81551332+mikemoreedu@users.noreply.github.com>
2023-03-06 21:41:14 -0500

    Update test_Github.py

--- test_Github.py
+++ test_Github.py
@@ -14,8 +14,16 @@ class github_test(unittest.TestCase):
         mock_requests.get.return_value = mock_response
         self.assertEqual(Github.get_git_repos_and_commits("mikemoreedu"), [('SSW567CircleCI', 2), ('Triangle567', 2)])
 
-    #def test_bad_input(self):
-     #   self.assertEqual(Github.get_git_repos_and_commits(1), "User ID must be a string")
+    @patch('Github.requests')
+    def test_bad_input(self):
+        mock_response = MagicMock()
+        mock_response.status_code = 404
+        mock_response.json.return_value = {
+            "Response": "Does not exist"}
+           
+        
+        mock_requests.get.return_value = mock_response
+        self.assertEqual(Github.get_git_repos_and_commits(1), "User ID must be a string")
     
     #def test_UserID(self):
      #   self.assertEqual(Github.get_git_repos_and_commits("notmikemoreedu"), "Invalid UserID")
